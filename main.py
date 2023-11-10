f262b05b856b502e54f47f16e2deb674b0093d75
Egore000 <egor.pakhomov.2002@gmail.com>
2023-11-10 23:18:09 +0700

    Реализовано управление и стрельба. Появился баг со взрывом

--- main.py
+++ main.py
@@ -6,13 +6,55 @@ pygame.init()
 
 screen = pygame.display.set_mode((Screen.WIDTH, Screen.HEIGHT))
 
-user = User(100, 100)
-while True:
+user = User(200, 200, 0)
+enemy = Enemy(100, 100, 0)
+
+pygame.init()
+
+screen = pygame.display.set_mode((Screen.WIDTH, Screen.HEIGHT))
+
+run = True
+bullets = []
+explosions = []
+while run:
+    current_time = pygame.time.get_ticks()
     screen.fill(Screen.COLOR)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            bullet, explosion = user.shoot()
+            bullets.append(bullet)
+            explosions.append(explosion)
+        
+    keys = pygame.key.get_pressed()        
+    
+    for explosion in explosions:
+        pygame.draw.rect(screen, settings.EXPLOSION_COLOR, explosion)
+
+    screen.fill(Screen.COLOR)
+
+    user.move(keys)
+    if keys[pygame.K_ESCAPE]:
+        run = False
+    elif keys[pygame.K_LEFT]:
+        Tank.rotate(user, -settings.delta)
+    elif keys[pygame.K_RIGHT]:
+        Tank.rotate(user, settings.delta)
+
+    for bullet in bullets:
+
+        if Screen.X_MIN <= bullet.x <= Screen.X_MAX and \
+            Screen.Y_MIN <= bullet.y <= Screen.Y_MAX: 
+            bullet.x += bullet.speed * np.sin(bullet.angle)
+            bullet.y -= bullet.speed * np.cos(bullet.angle)
+        # bullet.sprite = pygame.Rect(bullet.x, )
+            bullet.draw(screen)
+        else:
+            bullets.remove(bullet)
+
+    enemy.draw(screen)
     user.draw(screen)
     pygame.display.flip()
--- tools.py
+++ tools.py
@@ -1,11 +1,31 @@
 import settings
 import pygame
 import sys
+import numpy as np
+
 
 class Screen:
     WIDTH = settings.WIDTH
     HEIGHT = settings.HEIGHT
     COLOR = settings.BACKGROUND_COLOR
+    X_MIN = settings.X_MIN
+    X_MAX = settings.X_MAX
+    Y_MIN = settings.Y_MIN
+    Y_MAX = settings.Y_MAX
+
+class Bullet:
+    def __init__(self, x, y, angle):
+        self.x = x
+        self.y = y
+        self.angle = angle
+
+        self.speed = settings.BULLET_SPEED
+        self.sprite = pygame.Rect(x-3, y-3, 6, 6)
+        self.color = (255, 255, 0)
+
+    def draw(self, screen):
+        self.sprite = pygame.Rect(self.x, self.y, 5, 5)
+        pygame.draw.rect(screen, self.color, self.sprite)
 
 
 class Tank:
@@ -14,99 +34,113 @@ class Tank:
     GUN_WIDTH = settings.GUN_WIDTH
     GUN_LENGHT = settings.GUN_LENGHT
     TOWER_WIDTH = settings.TOWER_WIDTH
-    # def __init__(self, player):
-    #     self.color = type(player).COLOR
-    #     self.gun_color = (self.color[0]-10, self.color[0]-10, self.color[0]-10)
-    #     self.body = pygame.Rect(player.x - Tank.WIDTH // 2, player.y - Tank.HEIGHT // 2, Tank.WIDTH, Tank.HEIGHT)
-    #     self.gun = pygame.Rect(player.x - 3, player.y - Tank.HEIGHT // 2 - 40, 6, 60)
-    #     self.tower = pygame.()
 
-        # self.model = [
-        #     (pygame.Rect(player.x - Tank.WIDTH // 2, player.y - Tank.HEIGHT // 2, Tank.WIDTH, Tank.HEIGHT), self.color),
-        #     (pygame.Rect(player.x - 3, player.y - Tank.HEIGHT // 2 - 40, 6, 60), gun_color),
-        #     (pygame.circle())
-        # ]
+    def rotate(player, theta):
+        theta *= np.pi/180
+        player.angle += theta
+        player.angle -= round(player.angle/(2*np.pi)) * 2*np.pi
+    
+        return player
+
+    def move(player, keys):
+        if keys[pygame.K_w]:
+            player.y -= settings.velocity
+            player.w = Tank.WIDTH
+            player.h = Tank.HEIGHT
+            x = player.x - Tank.WIDTH//2
+            y = player.y - Tank.HEIGHT//2
+            # player.angle = 0
+            player.body = pygame.Rect(x, y, player.w, player.h)
+
+        elif keys[pygame.K_s]:
+            player.y += settings.velocity
+            player.w = Tank.WIDTH
+            player.h = Tank.HEIGHT
+            x = player.x - Tank.WIDTH//2
+            y = player.y - Tank.HEIGHT//2
+            # player.angle = np.pi
+            player.body = pygame.Rect(x, y, player.w, player.h)
+
+        elif keys[pygame.K_a]:
+            player.x -= settings.velocity
+            player.w = Tank.HEIGHT
+            player.h = Tank.WIDTH
+            x = player.x - Tank.HEIGHT//2
+            y = player.y - Tank.WIDTH//2
+            # player.angle = -np.pi/2
+            player.body = pygame.Rect(x, y, player.w, player.h)
+            
+        elif keys[pygame.K_d]:
+            player.x += settings.velocity
+            player.w = Tank.HEIGHT
+            player.h = Tank.WIDTH
+            x = player.x - Tank.HEIGHT//2
+            y = player.y - Tank.WIDTH//2
+            # player.angle = np.pi/2
+
+            player.body = pygame.Rect(x, y, player.w, player.h)
+        return player 
 
     def draw(player, screen):
         color = type(player).COLOR
-        r = color[0] - 40 if color[0] > 40 else color[0]
-        g = color[1] - 40 if color[1] > 40 else color[1]
-        b = color[2] - 40 if color[2] > 40 else color[2]
-
+        r = max(color[0] - 40, 0)
+        g = max(color[1] - 40, 0)
+        b = max(color[2] - 40, 0)
+        
         gun_color = (r, g, b)
-
-        body = pygame.Rect(player.x - Tank.WIDTH // 2,
-                        player.y - Tank.HEIGHT // 2,
-                        Tank.WIDTH, 
-                        Tank.HEIGHT)
-        gun = pygame.Rect(player.x - Tank.GUN_WIDTH // 2,
-                        player.y - Tank.HEIGHT // 2 - Tank.GUN_LENGHT/1.5,
-                        Tank.GUN_WIDTH,
-                        Tank.GUN_LENGHT)
-        tower = pygame.Rect(player.x - Tank.WIDTH // 2 + 5,
-                        player.y - Tank.HEIGHT // 2 + 10,
-                        Tank.TOWER_WIDTH,        
-                        Tank.TOWER_WIDTH,)        
-
-        pygame.draw.rect(screen, color, body)
-        pygame.draw.rect(screen, gun_color, gun)
+        
+        start = (player.x, player.y)
+        end = (player.x + Tank.GUN_LENGHT * np.sin(player.angle), player.y - Tank.GUN_LENGHT * np.cos(player.angle))
+                       
+        pygame.draw.rect(screen, color, player.body)
+        pygame.draw.line(screen, gun_color, start, end, Tank.GUN_WIDTH) 
         pygame.draw.circle(screen,
                         gun_color,
                         (player.x, player.y),
                         Tank.TOWER_WIDTH//2)
 
-class Enemy:
+    def shoot(player):
+        x = player.x + Tank.GUN_LENGHT * np.sin(player.angle)
+        y = player.y - Tank.GUN_LENGHT * np.cos(player.angle)
+        explosion = pygame.Rect(x-6, y-6, 12, 12)
+        return Bullet(x, y, player.angle), explosion
+
+
+
+class Enemy(Tank):
     COLOR = settings.ENEMY_COLOR
-    def __init__(self, x, y):
+    def __init__(self, x, y, angle):
         self.x = x
         self.y = y
-        # self.angle = angle
+        self.x0 = x - Tank.WIDTH//2
+        self.y0 = y - Tank.HEIGHT//2
+        self.w = Tank.WIDTH
+        self.h = Tank.HEIGHT
+
+        self.angle = angle * np.pi/180
         self.hp = 100
         self.damage = 30
+        
+        self.body = pygame.Rect(self.x - Tank.WIDTH//2, self.y - Tank.HEIGHT//2, self.w, self.h)
 
-    def draw(self, screen):
-        Tank.draw(self, screen)
 
-class User:
+class User(Tank):
     COLOR = settings.USER_COLOR
-    def __init__(self, x, y):
-        self.x = x
+    def __init__(self, x, y, angle):
+        self.x = x 
         self.y = y
-        # self.angle = angle
+        self.x0 = x - Tank.WIDTH//2
+        self.y0 = y - Tank.HEIGHT//2
+        self.w = Tank.WIDTH
+        self.h = Tank.HEIGHT
+
+        self.angle = angle * np.pi/180
         self.color = User.COLOR
         self.hp = 100
         self.exp = 0
+        
+        self.body = pygame.Rect(self.x - Tank.WIDTH//2, self.y - Tank.HEIGHT//2, self.w, self.h)
 
-    def move(self, key):
-        if key == pygame.K_w:
-            self.y -= settings.velocity
-        elif key == pygame.K_s:
-            self.y += settings.velocity
-        elif key == pygame.K_a:
-            self.x 
-
-    def draw(self, screen):
-        Tank.draw(self, screen)
 
 if __name__ == "__main__":
-    user = User(200, 200)
-    enemy = Enemy(100, 100)
-    
-    pygame.init()
-
-    screen = pygame.display.set_mode((Screen.WIDTH, Screen.HEIGHT))
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            
-
-        screen.fill(Screen.COLOR)
-        # for elem in tank.model:
-        #     pygame.draw.rect(screen, elem[1], elem[0])
-        enemy.draw(screen)
-        user.draw(screen)
-
-        pygame.display.flip()
+    pass
